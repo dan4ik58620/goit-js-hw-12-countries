4f0e18c48ed2026268c8c5d5356d3a40632b3b76
dan4ik58620 <danyakotkot@gmail.com>
2020-11-07 20:52:21 +0200

    end

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import css from "./css/style.css";
+import fetchCountries from './fetchCountries.js'
+import debounce from 'lodash.debounce';
+
+const input = document.getElementById("searchInput")
+
+input.addEventListener('input', debounce((e)=>{
+  fetchCountries(e.target.value)
+  input.value = ''
+  }, 700))
+
+
+
+
